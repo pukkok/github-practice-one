8c29601098fe8592f32d5556c8012cebcc2ec7b8
pukkok <krystalgggg@gmail.com>
2024-11-19 14:49:41 +0900

    notes를 박스안에 생성하기

--- index.js
+++ index.js
@@ -1,15 +1,37 @@
-const root = document.getElementById('root')
+import notes from "./notes.js"
 
+const root = document.getElementById('root')
 const canvas = document.createElement('canvas')
-canvas.width = window.innerWidth
-canvas.height = window.innerHeight
 root.append(canvas)
 
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
 const ctx = canvas.getContext('2d')
 
-function drawRect(color) {
+const size = 40
+
+function drawRect(note, color='cadetblue') {
+    const { x, y, size, noteName } = note
+    const centerX = x - size / 2
+    const centerY = y - size / 2
     ctx.fillStyle = color
-    ctx.fillRect(200, 200, 20, 20)
+    ctx.fillRect(centerX, centerY, size, size)
+
+    ctx.fillStyle = 'white'
+    ctx.font = '20px sans-serif'
+    ctx.textAlign = 'center'
+    ctx.textBaseline = 'middle'
+    ctx.fillText(noteName, x, y)
 }
 
-drawRect('red')
+const startPoint = canvas.width / 2 - notes.length / 2 * (size + 10)
+notes.forEach((note, idx) => {
+    const newNote = {
+        x : startPoint + idx * (size + 10),
+        y : canvas.height / 2,
+        size: size,
+        pitch : note.pitch,
+        noteName : note.koreanName
+    }
+    drawRect(newNote, note.color)
+})
