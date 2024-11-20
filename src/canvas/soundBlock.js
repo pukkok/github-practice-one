53e5b8a54218943c489fae5b1228dba000fcfadf
Pukkok <krystalgggg@gmail.com>
2024-11-20 22:55:58 +0900

    주석 추가

--- src/canvas/soundBlock.js
+++ src/canvas/soundBlock.js
@@ -1,3 +1,11 @@
+/**
+ * 
+ * @param {CanvasRenderingContext2D} ctx 
+ * @param {object} note 
+ * @param {string} color
+ * @description 
+ * 노트의 사이즈를 받아 네모 모형의 사운드 블럭을 생성합니다.
+ */
 function soundBlock(ctx, note, color='cadetblue') {
     const { x, y, size, noteName } = note
     const centerX = x - size / 2
