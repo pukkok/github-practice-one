c32ebc09c0726166620cde828b7902af7e703a60
pukkok <krystalgggg@gmail.com>
2024-11-21 18:11:01 +0900

    노트 파스텔톤 추가

--- src/data/notes.js
+++ src/data/notes.js
@@ -13,13 +13,13 @@ const notes = [
     // { color: "#0000FF", pitch: "G3", koreanName: "솔", englishName: "Sol" },
     // { color: "#4B0082", pitch: "A3", koreanName: "라", englishName: "La" },
     // { color: "#8A2BE2", pitch: "B3", koreanName: "시", englishName: "Si" },
-    { color: "#FF0000", pitch: "C4", koreanName: "도", englishName: "Do" },
-    { color: "#FF7F00", pitch: "D4", koreanName: "레", englishName: "Re" },
-    { color: "#FFFF00", pitch: "E4", koreanName: "미", englishName: "Mi" },
-    { color: "#00FF00", pitch: "F4", koreanName: "파", englishName: "Fa" },
-    { color: "#0000FF", pitch: "G4", koreanName: "솔", englishName: "Sol" },
-    { color: "#4B0082", pitch: "A4", koreanName: "라", englishName: "La" },
-    { color: "#8A2BE2", pitch: "B4", koreanName: "시", englishName: "Si" },
+    { color: "#F28B82", pitch: "C4", koreanName: "도", englishName: "Do" },
+    { color: "#FBAF85", pitch: "D4", koreanName: "레", englishName: "Re" },
+    { color: "#F2D769", pitch: "E4", koreanName: "미", englishName: "Mi" },
+    { color: "#A3D181", pitch: "F4", koreanName: "파", englishName: "Fa" },
+    { color: "#78C0E0", pitch: "G4", koreanName: "솔", englishName: "Sol" },
+    { color: "#93B0C3", pitch: "A4", koreanName: "라", englishName: "La" },
+    { color: "#C8A2C8", pitch: "B4", koreanName: "시", englishName: "Si" },
     // { color: "#FF0000", pitch: "C5", koreanName: "도", englishName: "Do" },
     // { color: "#FF7F00", pitch: "D5", koreanName: "레", englishName: "Re" },
     // { color: "#FFFF00", pitch: "E5", koreanName: "미", englishName: "Mi" },
@@ -30,3 +30,26 @@ const notes = [
 ]
 
 export default notes
+
+/**
+ * 진한 default
+ * 
+ * 빨 : "#FF0000"
+ * 주 : "#FF7F00"
+ * 노 : "#FFFF00"
+ * 초 : "#00FF00"
+ * 파 : "#0000FF"
+ * 남 : "#4B0082"
+ * 보 : "#8A2BE2" 
+ */
+
+/**
+ * 파스텔 톤 버전
+ * 빨: #F28B82
+ * 주: #FBAF85
+ * 노: #F2D769
+ * 초: #A3D181
+ * 파: #78C0E0 
+ * 남: #93B0C3
+ * 보: #C8A2C8
+ */
